aee88dfaacb39dc9f5385eff2ec9431e6e6001e4
Knayoung <kimjongusa@gmail.com>
2022-04-20 13:04:02 -0400

    Created computerPlay which is a function that lets the CPU choose randomly between rock, paper and scissors

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // computerPlay will be a function that returns Rock, Paper or Scissors
-// I will create an array containing these three options for which the CPU will choose from
+// I will create an array containing these three options for which 
+// the CPU will choose from
+
+function computerPlay() {
+
+const cpuSelection =  ['Rock', 'Paper', 'Scissors'];
+
+var randomSelection = cpuSelection[Math.floor(Math.random() * cpuSelection.length)];
+
+console.log(randomSelection)
+
+
+}
